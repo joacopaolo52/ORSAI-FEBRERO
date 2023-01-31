04a82daea3470f3de796cd041cc469cae1c4ced1
Joaquin Paolo <joaquinpaolo1@gmail.com>
2023-01-31 03:17:59 -0300

    big advance

--- js/app.js
+++ js/app.js
@@ -7,12 +7,6 @@ const tablaFixture = document.querySelector(`#tabla-fixture`);
 const goleadores = document.querySelector(`.goleadores`);
 const tablaGoleadores = document.querySelector(`#tabla-goleadores`);
 
-const vallas = document.querySelector(`.vallas`);
-const tablaVallas = document.querySelector(`#tabla-vallas`);
-
-const fairplay = document.querySelector(`.fairplay`);
-const tablaFairplay = document.querySelector(`#tabla-fairplay`);
-
 const sancionados = document.querySelector(`.sancionados`);
 const tablaSancionados = document.querySelector(`#tabla-sancionados`);
 
@@ -28,12 +22,6 @@ posiciones.addEventListener('click', () => {
 
     goleadores.setAttribute('class', 'item-torneo goleadores');
     tablaGoleadores.setAttribute(`style`, `display: none;`);
-
-    vallas.setAttribute('class', 'item-torneo vallas');
-    tablaVallas.setAttribute(`style`, `display: none;`);
-
-    fairplay.setAttribute('class', 'item-torneo fairplay');
-    tablaFairplay.setAttribute(`style`, `display: none;`);
 
     sancionados.setAttribute('class', 'item-torneo sancionados');
     tablaSancionados.setAttribute(`style`, `display: none;`);
@@ -52,12 +40,6 @@ fixture.addEventListener('click', () => {
     goleadores.setAttribute('class', 'item-torneo goleadores');
     tablaGoleadores.setAttribute(`style`, `display: none;`);
 
-    vallas.setAttribute('class', 'item-torneo vallas');
-    tablaVallas.setAttribute(`style`, `display: none;`);
-
-    fairplay.setAttribute('class', 'item-torneo fairplay');
-    tablaFairplay.setAttribute(`style`, `display: none;`);
-
     sancionados.setAttribute('class', 'item-torneo sancionados');
     tablaSancionados.setAttribute(`style`, `display: none;`);
 
@@ -73,58 +55,11 @@ goleadores.addEventListener('click', () => {
     goleadores.setAttribute('class', 'item-torneo goleadores active');
     tablaGoleadores.setAttribute(`style`, `display: table;`);
 
-    vallas.setAttribute('class', 'item-torneo vallas');
-    tablaVallas.setAttribute(`style`, `display: none;`);
-
-    fairplay.setAttribute('class', 'item-torneo fairplay');
-    tablaFairplay.setAttribute(`style`, `display: none;`);
-
     sancionados.setAttribute('class', 'item-torneo sancionados');
     tablaSancionados.setAttribute(`style`, `display: none;`);
 
 });
 
-vallas.addEventListener('click', () => {
-    posiciones.setAttribute('class', 'item-torneo posiciones');
-    tablaPosiciones.setAttribute(`style`, `display: none;`);
-
-    fixture.setAttribute('class', 'item-torneo fixture');
-    tablaFixture.setAttribute(`style`, `display: none;`);
-
-    goleadores.setAttribute('class', 'item-torneo goleadores');
-    tablaGoleadores.setAttribute(`style`, `display: none;`);
-
-    vallas.setAttribute('class', 'item-torneo vallas active');
-    tablaVallas.setAttribute(`style`, `display: table;`);
-
-    fairplay.setAttribute('class', 'item-torneo fairplay');
-    tablaFairplay.setAttribute(`style`, `display: none;`);
-
-    sancionados.setAttribute('class', 'item-torneo sancionados');
-    tablaSancionados.setAttribute(`style`, `display: none;`);
-
-});
-
-fairplay.addEventListener('click', () => {
-    posiciones.setAttribute('class', 'item-torneo posiciones');
-    tablaPosiciones.setAttribute(`style`, `display: none;`);
-
-    fixture.setAttribute('class', 'item-torneo fixture');
-    tablaFixture.setAttribute(`style`, `display: none;`);
-
-    goleadores.setAttribute('class', 'item-torneo goleadores');
-    tablaGoleadores.setAttribute(`style`, `display: none;`);
-
-    vallas.setAttribute('class', 'item-torneo vallas');
-    tablaVallas.setAttribute(`style`, `display: none;`);
-
-    fairplay.setAttribute('class', 'item-torneo fairplay active');
-    tablaFairplay.setAttribute(`style`, `display: table;`);
-
-    sancionados.setAttribute('class', 'item-torneo sancionados');
-    tablaSancionados.setAttribute(`style`, `display: none;`);
-
-});
 
 sancionados.addEventListener('click', () => {
     posiciones.setAttribute('class', 'item-torneo posiciones');
@@ -135,13 +70,6 @@ sancionados.addEventListener('click', () => {
 
     goleadores.setAttribute('class', 'item-torneo goleadores');
     tablaGoleadores.setAttribute(`style`, `display: none;`);
-
-    vallas.setAttribute('class', 'item-torneo vallas');
-    tablaVallas.setAttribute(`style`, `display: none;`);
-
-
-    fairplay.setAttribute('class', 'item-torneo fairplay');
-    tablaFairplay.setAttribute(`style`, `display: none;`);
 
     sancionados.setAttribute('class', 'item-torneo sancionados active');
     tablaSancionados.setAttribute(`style`, `display: table;`);
